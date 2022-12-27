f35f5ff0fc752e41431a628a22e78659c86d9d0c
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-12-27 16:22:19 -0500

    I finally did something

--- source/objects/Ornament.hx
+++ source/objects/Ornament.hx
@@ -10,14 +10,11 @@ class Ornament extends FlxSprite
 {
 	var ornaments:FlxTypedSpriteGroup<FlxSprite>;
 
-	public function new()
-	{
-		public static inline final COLORS:Array<String> = [
-			"yellow", "aqua", "blue", "cyan", "gold", "green", "indigo", "lime", "magenta", "mint", "orange", "pink",
-			"purple", "red", "rose", "scarlet", "teal", "white"
-		];
+	public var ornamentColor(default, set):String = '';
 
-        loadGraphic(Paths.image('ornaments/$COLORS'));
+	public function new(daColor:String = 'white')
+	{
+		set_ornamentColor(this.ornamentColor = daColor);
 
 		ornaments = new FlxTypedSpriteGroup<FlxSprite>();
 
@@ -25,5 +22,66 @@ class Ornament extends FlxSprite
 		{
 			ornaments.add(this);
 		}
+
+		width = 380;
+		height = 450;
+
+		super();
+	}
+
+	inline private function set_ornamentColor(v:String){
+		switch(v.toLowerCase()){
+			final target = 'ornaments/';
+			case 'yellow':
+				target += 'yellow';
+			case 'aqua':
+				target += 'aqua';
+			case 'blue':
+				target += 'blue';
+			case 'cyan':
+				target += 'cyan';
+			case 'gold':
+				target += 'gold';
+			case 'green':
+				target += 'green';
+			case 'indigo':
+				target += 'indigo';
+			case 'lime':
+				target += 'lime';
+			case 'magenta':
+				target += 'magenta';
+			case 'mint':
+				target += 'mint';
+			case 'orange':
+				target += 'orange';
+			case 'pink':
+				target += 'pink';
+			case 'purple':
+				target += 'purple';
+			case 'red':
+				target += 'red';
+			case 'rose':
+				target += 'rose';
+			case 'scarlet':
+				target += 'scarlet';
+			case 'teal':
+				target += 'teal';
+			case 'white':
+				target += 'white';
+			default:
+				trace('Error occured loading colors.');
+				// target += 'white';																	
+		}
+		loadGraphic(Paths.image(target));
+		updateHitbox();
+
+		return ornamentColor = v;
+	}
+
+	override function destroy(){
+		ornamentColor = null;
+		ornaments = null;
+
+		super.destroy();
 	}
 }
